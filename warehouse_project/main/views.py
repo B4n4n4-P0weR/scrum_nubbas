758910ea55af049ad5df4e5fa8ea87f9d83fb62e
B4n4n4-P0weR <vladtv2374@yandex.ru>
2024-05-28 15:25:26 +1000

    Добавил страницу "добавить заявку поставщику", пока пустую Сделал "список товаров" и "добавить товар" читабильнее

--- warehouse_project/main/views.py
+++ warehouse_project/main/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from .forms import ProductForm
 from .models import Product
 
+
 def home(request):
     return render(request, 'home.html')
 
@@ -22,56 +23,74 @@ def search(request, search_type):
     }
     return render(request, 'search.html', context)
 
+
 def reports(request):
     return render(request, 'reports.html')
+
 
 def report_needful_stuff(request):
     return render(request, 'report needful stuff.html')
 
+
 def report_sold_stuff(request):
     return render(request, 'report sold stuff.html')
 
+
 def report_stored_stuff(request):
     return render(request, 'report stored stuff.html')
+
 
 def supply_detail(request, supply_number):
     context = {'supply_number': supply_number}
     return render(request, 'supply one.html', context)
 
+
 def get_supply(request):
     supply_number = request.GET.get('supply_number')
     return redirect('supply_detail', supply_number=supply_number)
+
 
 def sale_detail(request, sale_number):
     context = {'sale_number': sale_number}
     return render(request, 'sale one.html', context)
 
+
 def get_sale(request):
     sale_number = request.GET.get('sale_number')
     return redirect('sale_detail', sale_number=sale_number)
 
+
 def sales_list(request):
     return render(request, 'sales list.html')
+
 
 def supplies_list(request):
     return render(request, 'supplies list.html')
 
+
 def sales_add(request):
     return render(request, 'sales add.html')
+
 
 def supplies_add(request):
     return render(request, 'supplies add.html')
 
+
 def supplier_requests(request):
     return render(request, 'supplier requests.html')
 
+def supplier_request_add(request):
+    return render(request, 'supplier request add.html')
+
+
 def product_list(request):
-    products = Product.objects.all()  # Получаем все товары из базы данных
+    products = Product.objects.all() # Получаем все товары из базы данных
     return render(request, 'product_list.html', {'products': products})
+
 
 def product_add(request):
     result = None
-    
+
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
@@ -83,6 +102,7 @@ def product_add(request):
 
     return render(request, 'product_add.html', {'form': form, 'result': result})
 
+
 def product_clear(request):
     Product.objects.all().delete()
-    return HttpResponse("<a href='http://127.0.0.1:8000' class='red'>Назад</a> Все продукты удалены")
+    return HttpResponse("<a href='http://127.0.0.1:8000/supplies/' class='red'>Назад</a> Все продукты удалены")
